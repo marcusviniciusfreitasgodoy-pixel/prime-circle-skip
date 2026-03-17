0b6d0948352faf5b7ce624c86f48f8056d22aa09
Skip <skip@usecurling.com>
2026-03-17 18:48:03 +0000

    Add professional affiliation to registration

--- supabase/functions/functions/index.ts
+++ supabase/functions/functions/index.ts
@@ -1,7 +1,12 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 
 Deno.serve(async (req: Request) => {
-  return new Response(JSON.stringify({ message: 'Hello from functions!' }), {
-    headers: { 'Content-Type': 'application/json' },
+  return new Response(JSON.stringify({ status: 'ok', message: 'Functions service is running' }), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    },
   })
 })
